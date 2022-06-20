eec66df404c5b2094312568f2116505d0b9c8b33
VitorOM2 <viktormarque2053@gmail.com>
2022-06-19 21:42:28 -0300

    [MODIFICADO] Adicionado comparação entre o chute e num secreto

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,11 @@
+print('=================================')
 print('Bem vindo ao jogo de Adivinhação!')
+print('=================================')
+
+numero_secreto = 8
+chute = int(input('Digite o seu número: '))
+
+if numero_secreto == chute:
+    print('Você acertou!')
+else:
+    print('Você errou!')
