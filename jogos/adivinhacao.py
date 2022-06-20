871410887ab92f72643b951e6e2de22a906d0224
VitorOM2 <viktormarque2053@gmail.com>
2022-06-19 23:17:31 -0300

    [MODIFICADO] Adicionado validações

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,11 +8,17 @@ total_tentativas = 3
 for rodada in range(1, total_tentativas + 1):
 
     print('\nTentativa {} de {}'.format(rodada, total_tentativas))
-    chute = int(input('\nDigite o seu número: '))
+    chute = int(input('\nDigite um número de 1 a 100: '))
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número de 1 a 100")
+        continue
+
     print('Seu número é: ', chute)
 
     if chute == numero_secreto:
         print('Você acertou!')
+        break
     else:
         if chute > numero_secreto:
             print('Você errou! O seu chute foi maior do que o número secreto')
