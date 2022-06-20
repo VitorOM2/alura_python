e295f132330837b7cc6ade9aa5149f74134c2f2e
VitorOM2 <viktormarque2053@gmail.com>
2022-06-19 22:11:26 -0300

    Adicionado dicas

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,9 +3,12 @@ print('Bem vindo ao jogo de Adivinhação!')
 print('=================================')
 
 numero_secreto = 8
-chute = int(input('Digite o seu número: '))
+chute          = int(input('Digite o seu número: '))
 
-if numero_secreto == chute:
+if chute == numero_secreto:
     print('Você acertou!')
 else:
-    print('Você errou!')
+    if chute > numero_secreto:
+        print('Você errou! O seu chute foi maior do que o número secreto')
+    else:
+        print('Você errou! O seu chute foi menor do que o número secreto')
