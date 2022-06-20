36d8d446c712bcce22b699a1102739e94e2615b5
VitorOM2 <viktormarque2053@gmail.com>
2022-06-19 22:43:00 -0300

    Adicionado sistemas de tentativas

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,12 +3,23 @@ print('Bem vindo ao jogo de Adivinhação!')
 print('=================================')
 
 numero_secreto = 8
-chute          = int(input('Digite o seu número: '))
+rodada = 1
+total_tentativas = 3
 
-if chute == numero_secreto:
-    print('Você acertou!')
-else:
-    if chute > numero_secreto:
-        print('Você errou! O seu chute foi maior do que o número secreto')
+while rodada <= total_tentativas:
+
+    chute = int(input('Digite o seu número: '))
+    print('Tentativa:', rodada, 'de', total_tentativas)
+    print('Seu número é: ', chute)
+
+    if chute == numero_secreto:
+        print('Você acertou!')
     else:
-        print('Você errou! O seu chute foi menor do que o número secreto')
+        if chute > numero_secreto:
+            print('Você errou! O seu chute foi maior do que o número secreto')
+        else:
+            print('Você errou! O seu chute foi menor do que o número secreto')
+
+    rodada = rodada + 1
+
+print('Fim de Jogo!')
