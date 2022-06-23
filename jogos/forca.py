bfe4b1bc818e202226a165e323f7d23f63afd822
VitorOM2 <viktormarque2053@gmail.com>
2022-06-22 21:40:35 -0300

    [MODIFICADO] Primeira versão do jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,5 +1,14 @@
 def jogar():
-    print("forca")
+    print('=================================')
+    print('   Bem vindo ao jogo da forca!   ')
+    print('=================================')
+
+    palavra_secreta = "banana"
+    enforcou = False
+    ganhou   = False
+
+    while not enforcou and not ganhou:
+        print("Jogando")
 
 
 if __name__ == "__main__":
