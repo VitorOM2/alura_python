d485d9eee0dad642e36511fd9d96acfece29a94e
VitorOM2 <viktormarque2053@gmail.com>
2022-06-23 23:37:41 -0300

    [MODIFICADO] Organização do código

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,7 +3,7 @@ def jogar():
     print('   Bem vindo ao jogo da forca!   ')
     print('=================================')
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_certas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     ganhou   = False
@@ -12,16 +12,16 @@ def jogar():
     print(letras_certas)
 
     while not enforcou and not ganhou:
-        chute = input("Qual letra? ").strip()
+        chute = input("Qual letra? ").strip().upper()
         index = 0
 
         if chute not in palavra_secreta:
             erros += 1
         else:
             for letra in palavra_secreta:  # Loop para iterar a string
-                if chute.upper() == letra.upper():
+                if chute == letra:
                     letras_certas[index] = letra
-                index = index + 1  # Acrescenta uma posição no index para cada letra
+                index += 1  # Acrescenta uma posição no index para cada letra
 
         enforcou = erros == 6
         print(letras_certas)
