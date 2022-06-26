ef5fb0c41c0396cb249b102bc5999e4b88599eac
VitorOM2 <viktormarque2053@gmail.com>
2022-06-26 00:20:49 -0300

    [NOVO] conta.py

---
+++ oo/conta.py
@@ -0,0 +1,7 @@
+class Conta:
+
+    def __int__(self, numero, titular, saldo, limite):
+        self.numero  = numero
+        self.titular = titular
+        self.saldo   = saldo
+        self.limite  = limite
