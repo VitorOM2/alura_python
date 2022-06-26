c81534b31ef08e9e8f89d59628cca111cb5b638d
VitorOM2 <viktormarque2053@gmail.com>
2022-06-25 23:40:17 -0300

    [NOVO] teste.py

---
+++ oo/teste.py
@@ -0,0 +1,15 @@
+def cria_conta(numero, saldo, titular, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print("Saldo: {}".format(conta["saldo"]))
