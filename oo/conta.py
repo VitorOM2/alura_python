31b9770bd8ce70137c5dfd3d6edace1460651578
VitorOM2 <viktormarque2053@gmail.com>
2022-06-26 23:27:39 -0300

    [MODIFICADO] Atributos alterado para privados

--- oo/conta.py
+++ oo/conta.py
@@ -1,16 +1,16 @@
 class Conta:
 
     def __int__(self, numero, titular, saldo, limite):
-        self.numero  = numero
-        self.titular = titular
-        self.saldo   = saldo
-        self.limite  = limite
+        self.__numero  = numero
+        self.__titular = titular
+        self.__saldo   = saldo
+        self.__limite  = limite
 
     def extrato(self):
-        print("Extrato: {}".format(self.saldo))
+        print("Extrato: {}".format(self.__saldo))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
