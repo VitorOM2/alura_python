9309c9c0c63a4f13589facbacc9e27c852de0182
VitorOM2 <viktormarque2053@gmail.com>
2022-06-26 22:24:29 -0300

    [MODIFICADO] Adicionado métodos de de sacar, depositar e mostrar extrato

--- oo/conta.py
+++ oo/conta.py
@@ -5,3 +5,12 @@ class Conta:
         self.titular = titular
         self.saldo   = saldo
         self.limite  = limite
+
+    def extrato(self):
+        print("Extrato: {}".format(self.saldo))
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
