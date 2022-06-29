d6cfee8834ecccfb347bf81fe90da4f7c8c6d08f
VitorOM2 <viktormarque2053@gmail.com>
2022-06-28 23:13:02 -0300

    [MODIFICADO] Adicionado método construtor

--- oo2/filmes_series.py
+++ oo2/filmes_series.py
@@ -1,6 +1,9 @@
 class Filme:
-    pass
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano  = ano
+        self.duracao = duracao
 
 
-vingadores = Filme()
-print(vingadores)
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+print(vingadores.nome)
