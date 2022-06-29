477f17d9d484f919262e6d35c5842b9e910a6865
VitorOM2 <viktormarque2053@gmail.com>
2022-06-28 23:16:31 -0300

    [MODIFICADO] Adicionado classe serie

--- oo2/filmes_series.py
+++ oo2/filmes_series.py
@@ -5,5 +5,13 @@ class Filme:
         self.duracao = duracao
 
 
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano  = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(vingadores.nome)
