f33fd263b74372059072ab4f20224bedcaadc01c
VitorOM2 <viktormarque2053@gmail.com>
2022-06-29 11:22:24 -0300

    [MODIFICADO] Adicionado classe super

--- oo2/filmes_series.py
+++ oo2/filmes_series.py
@@ -1,56 +1,42 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.duracao = duracao
-        self.__like = 0
+class Programa():
+    def __init__(self,nome, ano):
+        self._nome = nome
+        self.ano   = ano
+        self._like = 0
 
     # =============== GETTERS ===============
     @property
     def like(self):
-        return self.__like
+        return self._like
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     # =============== SETTERS ===============
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome
+        self._nome = nome
 
     # =============== MÉTODOS: Variados ===============
     def dar_likes(self):
-        self.__like += 1
+        self._like += 1
 
 
-class Serie:
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__like = 0
-
-    # =============== GETTERS ===============
-    @property
-    def like(self):
-        return self.__like
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    # =============== SETTERS ===============
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
-    def dar_likes(self):
-        self.__like += 1
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'{vingadores.nome}')
+print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} - {vingadores.like}')
 
 himym = Serie('how i met your mother', 2005, 9)
-print(f'\nSérie: {himym.nome} \nAno de lançamento: {himym.ano} \nTemporadas: {himym.temporadas}')
+print(f'\nSérie: {himym.nome} \nAno de lançamento: {himym.ano} \nTemporadas: {himym.temporadas} - {himym.like}')
