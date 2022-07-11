f3c6ba181c4577f2f21ff20408284e5b3c6167c1
VitorOM2 <viktormarque2053@gmail.com>
2022-07-11 12:04:38 -0300

    [MODIFICADO] Adicionado fatiamento

--- string_python/main.py
+++ string_python/main.py
@@ -1,2 +1,7 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url             = "bytebank.com/cambio?moedaOrigem=real"
+url_base        = url[0:19]
+url_parametros  = url[20:36]
+
 print(url)
+print(url_base)
+print(url_parametros)
