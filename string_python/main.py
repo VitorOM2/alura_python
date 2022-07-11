2997527797fedae9a9ac6c511882002589a1bbed
VitorOM2 <viktormarque2053@gmail.com>
2022-07-11 15:11:38 -0300

    [MODIFICADO] Melhorado o fatiamento

--- string_python/main.py
+++ string_python/main.py
@@ -1,7 +1,17 @@
-url             = "bytebank.com/cambio?moedaOrigem=real"
-url_base        = url[0:19]
-url_parametros  = url[20:36]
+URL                 = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+INDICE_INTERROGACAO = URL.find('?')
+URL_BASE            = URL[:INDICE_INTERROGACAO]
+URL_PARAMETROS      = URL[INDICE_INTERROGACAO + 1:]
+PARAMETRO_BUSCA     = 'quantidade'
+INDICE_PARAMETRO    = URL_PARAMETROS.find(PARAMETRO_BUSCA)
+INDICE_VALOR        = INDICE_PARAMETRO + len(PARAMETRO_BUSCA) + 1
+INDICE_E_COMERCIAL  = URL_PARAMETROS.find('&', INDICE_VALOR)
+if INDICE_E_COMERCIAL == -1:
+    VALOR = URL_PARAMETROS[INDICE_VALOR:]
+else:
+    VALOR = URL_PARAMETROS[INDICE_VALOR:INDICE_E_COMERCIAL]
 
-print(url)
-print(url_base)
-print(url_parametros)
+print(URL)
+print(URL_BASE)
+print(PARAMETRO_BUSCA)
+print(VALOR)
