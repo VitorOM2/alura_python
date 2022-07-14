be2dd451122c10c5a7f1c86941907d0719f1d8c0
VitorOM2 <viktormarque2053@gmail.com>
2022-07-14 15:36:34 -0300

    [NOVO] exemplos_listas.py

---
+++ listas_e_tuples/exemplos_listas.py
@@ -0,0 +1,9 @@
+def faz_processamento_lista(lista = None):
+    if lista == None:
+        lista = list()
+    print(len(lista))
+    print(lista)
+    lista.append(8)
+
+
+faz_processamento_lista()
