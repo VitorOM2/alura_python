6ef2c139b5d2df7f59fb14d3712b150da7099124
VitorOM2 <viktormarque2053@gmail.com>
2022-07-14 16:57:01 -0300

    [MODIFICADO] Adicionado duas classes: "ContaPoupanca" e "ContaCorrente"

--- listas_e_tuples/heranca_poliformismo.py
+++ listas_e_tuples/heranca_poliformismo.py
@@ -4,13 +4,31 @@ class Conta:
         self._codigo = codigo
         self._saldo  = 0
 
-    def deposita(self, valor):
-        self._saldo += valor
-
     def __str__(self):
         return f"\n>> Código: {self._codigo}\n>> Saldo: {self._saldo}"
 
+    def deposita(self, valor):
+        self._saldo += valor
 
-conta_teste = Conta(1)
-conta_teste.deposita(500)
-print(conta_teste)
+
+class ContaPoupanca(Conta):
+
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+
+class ContaCorrente(Conta):
+
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+
+conta2 = ContaCorrente(2)
+conta3 = ContaPoupanca(3)
+
+conta2.deposita(500)
+conta3.deposita(500)
+
+print(conta2)
+print(conta3)
