ec5f50baf093abdfa09c5d58437c2280ffe61ac3
VitorOM2 <viktormarque2053@gmail.com>
2022-07-19 12:26:13 -0300

    [MODIFICADO] Criada máscara para o CPF

--- python_brasil/cpf.py
+++ python_brasil/cpf.py
@@ -5,12 +5,21 @@ class Cpf:
 
         if self.verifica_cpf(documento):  # Chama a função de verificar
             self.documento = documento    # Se o CPF for válido atribui o valor
-            print("ok")
         else:
             raise ValueError("CPF inválido!")  # Chama um erro se o CPF for inválido
+
+    def __str__(self):
+        return f" CPF: {self.format_cpf()}"  # Chama a função para criar a máscara
 
     def verifica_cpf(self, documento):  # Verificador
         if len(documento) == 11:        # Verifica o tamanho do CPF
             return True
         else:
             return False
+
+    def format_cpf(self):  # Define o formato da máscara
+        fatia1 = self.documento[:3]
+        fatia2 = self.documento[3:6]
+        fatia3 = self.documento[6:9]
+        fatia4 = self.documento[9:]
+        return f"{fatia1}.{fatia2}.{fatia3}-{fatia4}"
--- python_brasil/main.py
+++ python_brasil/main.py
@@ -2,3 +2,5 @@ from cpf import Cpf
 
 cpf = 86075244026
 documento = Cpf(cpf)
+
+print(documento)
