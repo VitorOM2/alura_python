6b958adb3df385e9effddf0d40d512d099bc9cf3
VitorOM2 <viktormarque2053@gmail.com>
2022-07-20 15:49:12 -0300

    [MODIFICADO] adicionado try except

--- input_output/principal.py
+++ input_output/principal.py
@@ -1,5 +1,8 @@
-arquivo = open('dados/contatos.csv',
-               encoding='latin_1')
-
-for linha in arquivo:
-    print(linha, end='')
+try:
+    with open('dados/contatos.csv', encoding='latin_1') as arquivo:
+        for linha in arquivo:
+            print(linha, end='')
+except FileNotFoundError:
+    print('Arquivo não encontrado')
+except PermissionError:
+    print('Sem permisão de escrita')
