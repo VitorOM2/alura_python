b9675e5856faba53e323a9f5194f6dd4f413fc2d
VitorOM2 <viktormarque2053@gmail.com>
2022-07-20 12:47:35 -0300

    [MODIFICADO] Correções de identação

--- input_output/principal.py
+++ input_output/principal.py
@@ -1,5 +1,5 @@
-arquivo = open('/home/vmarques/Alura_Python/input_output/dados/contatos.csv',
-                encoding='latin_1')
+arquivo = open('dados/contatos.csv',
+               encoding='latin_1')
 
 for linha in arquivo:
     print(linha, end='')
