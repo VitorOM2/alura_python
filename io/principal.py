815a4eb160212fd40a62daae9b11a0ab153842f2
VitorOM2 <viktormarque2053@gmail.com>
2022-07-21 10:56:07 -0300

    Testes de serialização json

--- io/principal.py
+++ io/principal.py
@@ -1,11 +1,13 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_para_contatos('/dados/contatos.csv')
-    
-    contatos_utils.contatos_para_pickle(contatos, '/dados/contatos.csv')
-    
-    contatos = contatos_utils.pickle_para_contatos('/dados/contatos.csv')
+    contatos = contatos_utils.csv_para_contatos('contatos copy.csv')
+
+    # contatos_utils.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+    # contatos = contatos_utils.pickle_para_contatos('dados/contatos.pickle')
+
+    contatos_utils.contatos_para_json(contatos, 'contatos copy.json')
+    contatos = contatos_utils.json_para_contatos('contatos copy.json')
 
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
