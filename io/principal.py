ad2842f5a842a81f712b2a930a6fe8a732bdb6a8
VitorOM2 <viktormarque2053@gmail.com>
2022-07-21 10:20:56 -0300

    [MODIFICADO] Adicionada funções do contatos_utils

--- io/principal.py
+++ io/principal.py
@@ -1,13 +1,15 @@
 import contatos_utils
 
 try:
-    contatos = contatos_utils.csv_para_contatos
-    ('/home/vmarques/Alura_Python/io/dados/contatos.csv')
+    contatos = contatos_utils.csv_para_contatos('/dados/contatos.csv')
     
+    contatos_utils.contatos_para_pickle(contatos, '/dados/contatos.csv')
+    
+    contatos = contatos_utils.pickle_para_contatos('/dados/contatos.csv')
+
     for contato in contatos:
         print(f'{contato.id} - {contato.nome} - {contato.email}')
-    
 except FileNotFoundError:
     print('Arquivo não encontrado')
 except PermissionError:
-    print('Sem permisão de escrita')
+    print('Sem permissão de escrita')
