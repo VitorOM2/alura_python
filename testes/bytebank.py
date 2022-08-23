5a3fc3663cbea167ddb1a7bcb3f8acf264d50ad1
vmarques <vmarques@lliege.com.br>
2022-08-23 16:29:51 -0300

    Adicionado type hints

--- testes/bytebank.py
+++ testes/bytebank.py
@@ -1,30 +1,31 @@
 from datetime import date
 
+
 class Funcionario:
-    def __init__(self, nome, data_nascimento, salario):
-        self._nome = nome
-        self._data_nascimento = data_nascimento
-        self._salario = salario
+    def __init__(self, nome: str, data_nascimento: str, salario: float):
+        self._nome: str = nome
+        self._data_nascimento: str = data_nascimento
+        self._salario: float = salario
 
     @property
-    def nome(self):
+    def nome(self) -> str:
         return self._nome
 
     @property
-    def salario(self):
+    def salario(self) -> str:
         return self._salario
 
-    def idade(self):
+    def idade(self) -> int:
         data_nasciemnto_quebrada = self._data_nascimento.split('/')
         ano_nascimento = data_nasciemnto_quebrada[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
 
-    def calcular_bonus(self):
+    def calcular_bonus(self) -> float:
         valor = self._salario * 0.1
         if valor > 1000:
             valor = 0
         return valor
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
